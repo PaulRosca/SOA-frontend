4085ccf85099b97dc9111c51e1a455be1aa54525
paulrosca <paul.rosca00@e-uvt.ro>
2024-02-09 00:52:43 +0200

    container: setup container frontend bundle

---
+++ container/src/App.tsx
@@ -0,0 +1,49 @@
+import React, { useContext } from "react";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import ReactDOM from "react-dom";
+import ProductList from "store/ProductList";
+import Cart from "store/Cart";
+import SignIn from "auth/SignIn";
+import SignUp from "auth/SignUp";
+import SignOut from "auth/SignOut";
+import Header from "./components/Header";
+import { UserProvider, UserContext } from "auth/Context";
+import { CartProvider } from "store/Context";
+
+import "./index.scss";
+
+const HeaderWrapper = (Element: any) => {
+  return (
+    <>
+      <Header></Header>
+      <Element />
+    </>
+  )
+}
+
+const App = () => {
+  const { user } = useContext(UserContext)
+  return (
+    <Router>
+      <Routes>
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signout" element={<SignOut />} />
+        <Route path="/cart" element={<Cart user={user}/>} />
+        <Route path="/" element={HeaderWrapper(ProductList)} />
+        <Route path="*" element={<Navigate replace to="/" />} />
+      </Routes>
+    </Router>
+  )
+};
+
+const WrappedApp = () => {
+  return (
+    <CartProvider>
+      <UserProvider>
+        <App />
+      </UserProvider>
+    </CartProvider>
+  )
+}
+ReactDOM.render(<WrappedApp />, document.getElementById("app"));
