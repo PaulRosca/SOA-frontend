6c1ff0b1d43724418bbdcd8f8d4bdf7ccb3a3000
paulrosca <paul.rosca00@e-uvt.ro>
2024-02-09 12:47:36 +0200

    store: fix orders style

--- store/src/pages/Orders.tsx
+++ store/src/pages/Orders.tsx
@@ -23,54 +23,56 @@ export default function Orders({ isAdmin }: Props) {
   }, []);
   return (
     <div className="flex justify-center mt-5">
-      {
-        orders.map((ord) => {
-          return (
-            <div className="w-1/2 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700" key={ord.id}>
-              <p>
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Order #{ord.id}</h5>
-              </p>
-              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Address: {ord.address}</p>
-              <ul>
-                {
-                  ord.products.map((prod) => (
-                    <li key={prod.id}>
-                      <div className="flex justify-between items-center">
-                        <div className="font-semibold">
-                          {prod.title}
+      <div className="flex flex-col w-1/2">
+        {
+          orders.map((ord) => {
+            return (
+              <div className="w-full p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mt-5" key={ord.id}>
+                <p>
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Order #{ord.id}</h5>
+                </p>
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Address: {ord.address}</p>
+                <ul>
+                  {
+                    ord.products.map((prod) => (
+                      <li key={prod.id}>
+                        <div className="flex justify-between items-center">
+                          <div className="font-semibold">
+                            {prod.title}
+                          </div>
+                          <div>
+                            {prod.description}
+                          </div>
+                          <div>
+                            {prod.price}€
+                          </div>
                         </div>
-                        <div>
-                          {prod.description}
-                        </div>
-                        <div>
-                          {prod.price}€
-                        </div>
+                      </li>
+                    ))
+                  }
+                </ul>
+                <div className="flex justify-between items-center mt-2">
+                  {
+                    isAdmin ? (
+                      <select data-te-select-init value={ord.status} onChange={(e) => updateStatus(ord.id, e.target.value)}>
+                        <option value="processing">processing</option>
+                        <option value="shipped">shipped</option>
+                      </select>
+                    ) : (
+                      <div className={`rounded py-1 px-2 text-white ${ord.status === "processing" ? "bg-gray-500" : "bg-green-500"}`}>
+                        {ord.status}
                       </div>
-                    </li>
-                  ))
-                }
-              </ul>
-              <div className="flex justify-between items-center mt-2">
-                {
-                  isAdmin ? (
-                    <select data-te-select-init value={ord.status} onChange={(e) => updateStatus(ord.id, e.target.value)}>
-                      <option value="processing">processing</option>
-                      <option value="shipped">shipped</option>
-                    </select>
-                  ) : (
-                    <div className={`rounded py-1 px-2 text-white ${ord.status === "processing" ? "bg-gray-500" : "bg-green-500"}`}>
-                      {ord.status}
-                    </div>
-                  )
-                }
-                <div>
-                  {ord.timestamp}
+                    )
+                  }
+                  <div>
+                    {ord.timestamp}
+                  </div>
                 </div>
               </div>
-            </div>
-          );
-        })
-      }
+            );
+          })
+        }
+      </div>
     </div >
   );
 }
